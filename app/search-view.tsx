62145d4f06add01e56a5ed2fc27748f4c22c94ae
LibreFact <sheikhlipu9@proton.me>
2025-03-04 03:12:50 +0600

    Update search-view.tsx

--- app/search-view.tsx
+++ app/search-view.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import React, { useEffect, useState, useRef } from 'react';
+import { useEffect, useLayoutEffect, useState, useRef } from 'react';
 import axios from 'axios';
 import { useInView } from "react-intersection-observer";
 import { useTheme } from 'next-themes';
